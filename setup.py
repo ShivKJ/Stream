bdc198c5722541af17f011f25a54c676b4c4f838
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-09-16 07:28:24 +0530

    adding file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='basic',
+        version='1.0',
+        packages=['script', 'configuration'],
+        url='https://bitbucket.org/ShivKJ/learninggit',
+        license='',
+        author='shiv',
+        author_email='ShivKJ@gmail.com',
+        description='Learning pip and git',
+        long_description='long_description',
+        python_requires='>=3'
+)
