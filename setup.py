f5537995fb5a5692a322c95865d06c291147dfa0
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-09-16 08:43:17 +0530

    modifying setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
         name='basic',
         version='1.0',
-        packages=['script', 'configuration'],
+        packages=['script', 'utility', 'configuration'],
         url='https://bitbucket.org/ShivKJ/learninggit',
-        license='',
-        author='shiv',
+        license='SKJ',
+        author='Shiv',
         author_email='ShivKJ@gmail.com',
         description='Learning pip and git',
         long_description='long_description',
-        python_requires='>=3'
+        python_requires='>=3',
+        platforms='ubuntu'
 )
