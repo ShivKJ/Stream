7b5eff034978bf710c0f8b7ef37d47aee5f504de
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-09-17 12:16:15 +0530

    adding classUtils and inhe

---
+++ exp/effective_python/inheriting_named_tuple.py
@@ -0,0 +1,39 @@
+from functools import wraps
+
+from utility.class_utils import update_with_values_and_constructor_arg_name
+
+
+def p(f):
+    @wraps(f)
+    def get(cls, *args, **kwargs):
+        obj = object.__new__(cls)
+        update_with_values_and_constructor_arg_name(obj, *args, **kwargs)
+        return obj
+
+    return get
+
+
+class A:
+    def __init__(self, a, *, e=1, d=2, f):
+        print('init')
+        pass
+
+    @p
+    def __new__(cls, *args, **kwargs):
+        pass
+
+
+a = A(10, e='shiv', f='jaiswal')
+print(a.__dict__)
+
+
+class B:
+    pass
+
+
+class C(B):
+    pass
+
+
+print(B.mro())
+print(B.__subclasses__())
