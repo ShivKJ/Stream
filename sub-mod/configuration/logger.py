61868aae6d690dfe257366913eda2d78f1870d2a
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-09-17 23:09:08 +0530

    adding sub mog

---
+++ sub-mod/configuration/logger.py
@@ -0,0 +1,31 @@
+from logging import getLogger, Formatter, StreamHandler
+from logging.handlers import RotatingFileHandler
+from sys import stdout
+
+from configuration.config import LOG_FORMAT, LOG_FILE, DEFAULT_LOG_LEVEL, \
+    RF_HANDLER_LEVEL, LOG_FILE_SIZE, BACKUP_COUNT, STREAM_HANDLER_LEVLE
+
+
+def initialize_logger(logger_name):
+    logger = getLogger(logger_name)
+    logger.setLevel(DEFAULT_LOG_LEVEL)
+    fmt = Formatter(LOG_FORMAT)
+
+    # ---------------Adding File Handler--------------------
+    fh = RotatingFileHandler(LOG_FILE,
+                             maxBytes=LOG_FILE_SIZE,
+                             backupCount=BACKUP_COUNT)
+    fh.setLevel(RF_HANDLER_LEVEL)
+    fh.setFormatter(fmt)
+    logger.addHandler(fh)
+
+    # -----------------Adding Stream Handler-----------------
+    sh = StreamHandler(stdout)
+    sh.setFormatter(fmt)
+    sh.setLevel(STREAM_HANDLER_LEVLE)
+    logger.addHandler(sh)
+
+    return logger
+
+
+logger = initialize_logger(__name__)
