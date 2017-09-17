634df93e6c983d2db10f5c3bc8061e9ad2f58512
ShivKJ <shivkrishna.jaiswal@delhivery.com>
2017-09-17 13:01:01 +0530

    creating decorator which, when used with __init__, sets object field's names same as constructor's args name

--- exp/effective_python/inheriting_named_tuple.py
+++ exp/effective_python/inheriting_named_tuple.py
@@ -1,25 +1,9 @@
-from functools import wraps
-
-from utility.class_utils import update_with_values_and_constructor_arg_name
-
-
-def p(f):
-    @wraps(f)
-    def get(cls, *args, **kwargs):
-        obj = object.__new__(cls)
-        update_with_values_and_constructor_arg_name(obj, *args, **kwargs)
-        return obj
-
-    return get
+from utility.class_utils import constructor_setter
 
 
 class A:
+    @constructor_setter
     def __init__(self, a, *, e=1, d=2, f):
-        print('init')
-        pass
-
-    @p
-    def __new__(cls, *args, **kwargs):
         pass
 
 
