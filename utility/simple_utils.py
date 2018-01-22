529dc97cc38148200b107c2f0a94572d1d240979
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-01-23 04:54:48 +0530

    organizing imports

--- utility/simple_utils.py
+++ utility/simple_utils.py
@@ -1,10 +1,3 @@
-from psycopg2 import connect
-from psycopg2.extensions import connection
-from psycopg2.extras import DictConnection
-
-from utility.class_utils import constructor_setter
-
-
 def dump_obj(obj, file_path, indend=1):
     with open(file_path, 'w') as f:
         from json import dump
@@ -15,8 +8,6 @@ def load_obj(file_name):
     with open(file_name) as f:
         from json import load
         return load(f)
-
-
 
 
 def filter_transform(l: list, filt, transform):
