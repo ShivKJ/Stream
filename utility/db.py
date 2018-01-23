191012d246982fbf8d611c841ea16d108426b4bf
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-01-23 05:34:35 +0530

    adding return type

--- utility/db.py
+++ utility/db.py
@@ -1,19 +1,20 @@
 from psycopg2 import connect
+from psycopg2.extensions import connection
 from psycopg2.extras import DictConnection
 
 
 class DB:
-    def __init__(self, *, db, username, password, host='localhost', port=5432):
-        self.dbname = db
-        self.username = username
+    def __init__(self, *, dbname, user, password, host='localhost', port=5432):
+        self.dbname = dbname
+        self.user = user
         self.password = password
-        self.host = host,
+        self.host = host
         self.port = port
 
     @property
-    def dict_conn(self):
+    def dict_conn(self) -> connection:
         return connect(**self.__dict__, connection_factory=DictConnection)
 
     @property
-    def conn(self):
+    def conn(self) -> connection:
         return connect(**self.__dict__)
