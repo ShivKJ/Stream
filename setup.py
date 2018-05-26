b29bc2d53186e60bbd862c32697f26be5e458c0a
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-05-26 15:18:55 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='basic',
+    version='1.0',
+    packages=['utility'],
+    url='https://bitbucket.org/ShivKJ/learninggit',
+    license='SKJ',
+    author='Shiv',
+    author_email='None@gmail.com',
+    description='basic utility functions',
+    python_requires='>=3.6',
+    platforms='ubuntu'
+)
