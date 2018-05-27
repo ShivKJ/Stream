33f341ff3ca16a64e80199ff7388126a6d1a3eab
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-05-27 12:42:27 +0530

    adding more stream function

--- utility/stream.py
+++ utility/stream.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from itertools import islice
+from itertools import islice, chain
 from typing import Iterable, TypeVar, Generic, Sequence, Dict, Any
 
 from utility.utils import get_functions_clazz, identity
@@ -35,6 +35,20 @@ def _close_stream(func):
     return f
 
 
+class Optional(Generic[T]):
+    def __init__(self, data: T):
+        self.data = data
+
+    def present(self):
+        return self.data is not None
+
+    def get(self):
+        return self.data
+
+
+EMPTY = Optional(None)
+
+
 class Stream(Generic[T]):
 
     def __init__(self, data: Iterable[T]):
@@ -60,25 +74,7 @@ class Stream(Generic[T]):
         return self
 
     @_check_stream
-    @_close_stream
-    def count(self) -> int:
-        """
-        :return: number of elements in Stream
-        """
-        return sum(1 for _ in self._pointer)
-
-    @_check_stream
-    @_close_stream
-    def min(self, comp=None) -> T:
-        return min(self._pointer, key=comp) if comp else min(self._pointer)
-
-    @_check_stream
-    @_close_stream
-    def max(self, comp=None) -> T:
-        return max(self._pointer, key=comp) if comp else max(self._pointer)
-
-    @_check_stream
-    def sort(self, comp=None) -> 'Stream[T]':
+    def sorted(self, comp=None) -> 'Stream[T]':
         self._pointer = sorted(self._pointer, key=comp)
         return self
 
@@ -88,8 +84,63 @@ class Stream(Generic[T]):
         return self
 
     @_check_stream
+    def peek(self, consumer) -> 'Stream[T]':
+        self._pointer = Stream.consumer_wrapper(consumer)(self._pointer)
+        return self
+
+    @staticmethod
+    def consumer_wrapper(consumer):
+
+        @wraps(consumer)
+        def func(generator: Iterable[T]) -> Iterable[T]:
+            for g in generator:
+                consumer(g)
+                yield g
+
+        return func
+
+    @_check_stream
+    def skip(self, n) -> 'Stream[T]':
+        self._pointer = islice(self._pointer, n, None)
+        return self
+
+    @_check_stream
+    def flat_map(self) -> 'Stream[T]':
+        self._pointer = chain.from_iterable(self._pointer)
+        return self
+
+    @_check_stream
     @_close_stream
-    def groupBy(self, key_hasher, value_mapper=identity) -> Dict[Any, Sequence[T]]:
+    def partition(self, mapper=bool) -> Dict[bool, Sequence[T]]:
+        return self.group_by(mapper)
+
+    @_check_stream
+    @_close_stream
+    def count(self) -> int:
+        """
+        :return: number of elements in Stream
+        """
+        return sum(1 for _ in self._pointer)
+
+    @_check_stream
+    @_close_stream
+    def min(self, comp=None) -> Optional[Any]:
+        try:
+            return min(self._pointer, key=comp) if comp else min(self._pointer)
+        except ValueError:
+            return EMPTY
+
+    @_check_stream
+    @_close_stream
+    def max(self, comp=None) -> Optional[Any]:
+        try:
+            return Optional(max(self._pointer, key=comp) if comp else max(self._pointer))
+        except ValueError:
+            return EMPTY
+
+    @_check_stream
+    @_close_stream
+    def group_by(self, key_hasher, value_mapper=identity) -> Dict[Any, Sequence[T]]:
         out = {}
 
         for elem in self._pointer:
@@ -112,10 +163,6 @@ class Stream(Generic[T]):
 
         return out
 
-    @_check_stream
-    def partition(self, mapper=bool) -> Dict[bool, Sequence[T]]:
-        return self.groupBy(mapper)
-
     @staticmethod
     def _update(d: dict, k, v):
         if k not in d:
@@ -136,6 +183,44 @@ class Stream(Generic[T]):
     @_close_stream
     def as_seq(self, seq_clazz=list) -> Sequence[T]:
         return seq_clazz(self._pointer)
+
+    @_check_stream
+    @_close_stream
+    def all(self, predicate=identity) -> bool:
+        return all(map(predicate, self._pointer))
+
+    @_check_stream
+    @_close_stream
+    def any(self, predicate=identity) -> bool:
+        return any(map(predicate, self._pointer))
+
+    @_check_stream
+    @_close_stream
+    def none_match(self, predicate=identity) -> bool:
+        return not self.any(predicate)
+
+    @_check_stream
+    @_close_stream
+    def find_first(self) -> Optional[Any]:
+        for g in self._pointer:
+            return Optional(g)
+
+        return EMPTY
+
+    @_check_stream
+    @_close_stream
+    def for_each(self, consumer):
+        for g in self._pointer:
+            consumer(g)
+
+    @_check_stream
+    @_close_stream
+    def reduce(self, initial_point: T, bi_func) -> Optional[T]:
+
+        for g in self._pointer:
+            initial_point = bi_func(initial_point, g)
+
+        return initial_point
 
 
 if __name__ == 'utility.stream':
