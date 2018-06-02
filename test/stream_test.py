582a3b533310394f55222ce1313dbb1c6369ef77
ShivKJ <shivkrishnajaiswal@gmail.com>
2018-06-02 09:16:40 +0530

    correcting test file

--- test/stream_test.py
+++ test/stream_test.py
@@ -1,14 +1,14 @@
 import unittest
 
-from stream import Stream
+from stream import Stream, EMPTY
 
 
 class StreamTest(unittest.TestCase):
     def test_reduce(self):
         def _sum(a, b): return a + b
 
-        print(Stream(range(3, 9)).reduce(_sum))
-        self.assertEqual(Stream(range(3, 9)).reduce(_sum), sum(range(3, 9)))
+        self.assertEqual(Stream(range(3, 9)).reduce(_sum).get(), sum(range(3, 9)))
+        self.assertTrue(Stream(range(0)).reduce(_sum) is EMPTY)
 
 
 if __name__ == '__main__':
